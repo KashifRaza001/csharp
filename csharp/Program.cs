8ce348c8941ebf4377c7adbe2b31683fc50cbf6c
Kashif Raza <kashifrazatv@gmail.com>
2025-07-14 10:28:01 +0500

    Add project files.

---
+++ csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey it's me");
+            Console.ReadKey();
+        }
+    }
+}
